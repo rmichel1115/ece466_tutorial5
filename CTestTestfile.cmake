88296c7a1cb79bb330804fae5d9c016a6128eb45
rmichel1115 <rcmichel@ncsu.edu>
2025-04-08 00:25:48 -0400

    Create CTestTestfile.cmake

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for
+# Source directory: /ece566/tutorials/5/C++
+# Build directory: /ece566/tutorials/5/C++/build
+#
+# This file includes the relevant testing commands required for
+# testing this directory and lists subdirectories to be tested as well.
+add_test(RunDCE "/ece566/tutorials/5/C++/build/dce" "test0.bc" "test0-dce.bc")   
+set_tests_properties(RunDCE PROPERTIES  _BACKTRACE_TRIPLES "/ece566/tutorials/5/C++/CMakeLists.txt;30;add_test;/ece566/tutorials/5/C++/CMakeLists.txt;0;") 
